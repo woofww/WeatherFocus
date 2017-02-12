ed008ba9e472d2019f9692d3cc4456dffe8e7d14
woofww <woof0829@gmail.com>
2017-02-12 20:08:58 +0800

    Initial commit

---
+++ app/src/main/java/com/woof/weatherfocus/base/Constant.java
@@ -0,0 +1,12 @@
+package com.woof.weatherfocus.base;
+
+/**
+ * Created by Woof on 2/12/2017.
+ */
+
+public class Constant {
+    /**
+     * This class response for storing some unchanged data, like api key
+     */
+    public static final String Key = "3f3f62699c3d47fe91ad64ca939a83f8";
+}
